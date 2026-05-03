d04535c4d3169aa0047be68e53dda216a4fa8fce
gururajkl <gururajragavendra7@gmail.com>
2026-05-03 20:37:11 +0530

    Create IUsersRepository contract in core project

---
+++ Ecommerce.UsersService/Ecommerce.Core/RepositoryContracts/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Core.Entities;
+
+namespace Ecommerce.Core.RepositoryContracts;
+
+public interface IUsersRepository
+{
+    /// <summary>
+    /// Method to add a new user to the repository.
+    /// </summary>
+    Task<ApplicationUser?> AddUserAsync(ApplicationUser user);
+
+    /// <summary>
+    /// Method to get a user by their email and password.
+    /// </summary>
+    Task<ApplicationUser?> GetUserByEmailAndPasswordAsync(string? email, string? password);
+}
