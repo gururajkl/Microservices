336e201797af4edf8efdc2e09bd5c353aeb2487a
gururajkl <gururajragavendra7@gmail.com>
2026-05-03 18:34:53 +0530

    Register and add services and map them
    - Add controllers
    - Add auth middlewares
    - Support routing

--- Ecommerce.UsersService/Ecommerce.API/Program.cs
+++ Ecommerce.UsersService/Ecommerce.API/Program.cs
@@ -11,8 +11,20 @@ builder.Services.AddInfrastructure();
 // Add core services to the dependency injection container.
 builder.Services.AddCore();
 
+// Add controllers to the application pipeline.
+builder.Services.AddControllers();
+
+// Build the web application.
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Add routing.
+app.UseRouting();
+
+// Add authentication and authorization middleware.
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Map controller endpoints.
+app.MapControllers();
 
 app.Run();
