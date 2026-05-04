1a5cedc512dc5f68956de5817ad41cab52d41c80
gururajkl <gururajragavendra7@gmail.com>
2026-05-04 09:26:04 +0530

    Create IUsersService contract in core project

---
+++ Ecommerce.UsersService/Ecommerce.Core/ServiceContracts/IUsersService.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Core.DTO;
+
+namespace Ecommerce.Core.ServiceContracts;
+
+/// <summary>
+/// User service contract for handling user authentication and registration.
+/// </summary>
+public interface IUsersService
+{
+    /// <summary>
+    /// Method to authenticate a user based on the provided login request.
+    /// </summary>
+    Task<AuthenticationResponse?> LoginAsync(LoginRequest request);
+
+    /// <summary>
+    /// Method to register a new user based on the provided registration request.
+    /// </summary>
+    Task<AuthenticationResponse?> RegisterAsync(RegisterRequest request);
+}
