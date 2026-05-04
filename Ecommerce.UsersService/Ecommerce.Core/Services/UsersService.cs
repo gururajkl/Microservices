4df47f72fb5e4e89b8a39e814e4574d0d49e7c29
gururajkl <gururajragavendra7@gmail.com>
2026-05-04 09:34:36 +0530

    Add implementation of IUsersService in core

---
+++ Ecommerce.UsersService/Ecommerce.Core/Services/UsersService.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Core.DTO;
+using Ecommerce.Core.Entities;
+using Ecommerce.Core.RepositoryContracts;
+using Ecommerce.Core.ServiceContracts;
+
+namespace Ecommerce.Core.Services;
+
+internal class UsersService(IUsersRepository repository) : IUsersService
+{
+    public async Task<AuthenticationResponse?> LoginAsync(LoginRequest request)
+    {
+        ApplicationUser? user = await repository.GetUserByEmailAndPasswordAsync(request.Email, request.Password);
+
+        if (user is null) return null;
+
+        return new(user.UserId, user.Email, user.PersonName, user.Gender, "dummyToken", true);
+    }
+
+    public async Task<AuthenticationResponse?> RegisterAsync(RegisterRequest request)
+    {
+        ApplicationUser userToAdd = new()
+        {
+            Email = request.Email,
+            Password = request.Password,
+            PersonName = request.PersonName,
+            Gender = request.Gender?.ToString()
+        };
+
+        ApplicationUser? newUser = await repository.AddUserAsync(userToAdd);
+
+        if (newUser is null) return null;
+
+        return new(newUser.UserId, newUser.Email, newUser.PersonName, newUser.Gender, "dummyToken", true);
+    }
+}
