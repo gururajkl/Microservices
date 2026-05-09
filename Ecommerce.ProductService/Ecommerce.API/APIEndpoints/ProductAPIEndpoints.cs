03b65b9c1ccd1c8cc2aff58f33f448128acfe06e
gururajkl <gururajragavendra7@gmail.com>
2026-05-09 22:00:24 +0530

    Add couple of get minimal api endpoints for product microservice

--- Ecommerce.ProductService/Ecommerce.API/APIEndpoints/ProductAPIEndpoints.cs
+++ Ecommerce.ProductService/Ecommerce.API/APIEndpoints/ProductAPIEndpoints.cs
@@ -7,6 +7,20 @@ public static class ProductAPIEndpoints
     /// </summary>
     public static IEndpointRouteBuilder MapProductAPIEndpoints(this IEndpointRouteBuilder app)
     {
+        // GET, /api/products.
+        app.MapGet("/api/products", async (IProductsService productsService) =>
+        {
+            List<ProductResponse?> products = await productsService.GetProductsAsync();
+            return Results.Ok(products);
+        });
+
+        // GET, /api/products/search/product-id/ID.
+        app.MapGet("/api/products/search/product-id/{productID}", async (IProductsService productsService, Guid productID) =>
+        {
+            ProductResponse? product = await productsService.GetProductByConditionAsync(p => p.ProductID == productID);
+            return Results.Ok(product);
+        });
+
         return app;
     }
 }
--- Ecommerce.ProductService/Ecommerce.API/GlobalUsings.cs
+++ Ecommerce.ProductService/Ecommerce.API/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using Ecommerce.API.APIEndpoints;
 global using Ecommerce.API.Handlers;
 global using Ecommerce.BusinessLogicLayer;
+global using Ecommerce.BusinessLogicLayer.DTO;
+global using Ecommerce.BusinessLogicLayer.ServiceContracts;
 global using Ecommerce.DataAccessLayer;
 global using Ecommerce.DataAccessLayer.Entities;
