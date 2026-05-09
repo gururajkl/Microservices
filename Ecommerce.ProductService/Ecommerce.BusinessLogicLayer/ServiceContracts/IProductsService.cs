582abe0e0e0eec789619b87b5ba6d5b0221f8b32
gururajkl <gururajragavendra7@gmail.com>
2026-05-09 19:55:58 +0530

    Fix arg names

--- Ecommerce.ProductService/Ecommerce.BusinessLogicLayer/ServiceContracts/IProductsService.cs
+++ Ecommerce.ProductService/Ecommerce.BusinessLogicLayer/ServiceContracts/IProductsService.cs
@@ -9,7 +9,7 @@ public interface IProductsService
     Task<List<ProductResponse?>> GetProductsAsync();
     Task<List<ProductResponse?>> GetProductsByConditionAsync(Expression<Func<Product, bool>> expression);
     Task<ProductResponse?> GetProductByConditionAsync(Expression<Func<Product, bool>> expression);
-    Task<ProductResponse?> AddProductAsync(ProductAddRequest product);
-    Task<ProductResponse?> UpdateProductAsync(ProductUpdateRequest product);
+    Task<ProductResponse?> AddProductAsync(ProductAddRequest productAddRequest);
+    Task<ProductResponse?> UpdateProductAsync(ProductUpdateRequest productUpdateRequest);
     Task<bool> DeleteProductAsync(Guid productID);
 }
