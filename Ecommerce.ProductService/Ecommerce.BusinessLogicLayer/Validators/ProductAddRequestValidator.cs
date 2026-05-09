90e61c112e50e34e77ed485e8413f38e66f42647
gururajkl <gururajragavendra7@gmail.com>
2026-05-09 16:35:25 +0530

    Create validators for product DTO's

---
+++ Ecommerce.ProductService/Ecommerce.BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace Ecommerce.BusinessLogicLayer.Validators;
+
+public class ProductAddRequestValidator : AbstractValidator<ProductAddRequest>
+{
+    public ProductAddRequestValidator()
+    {
+        RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product name cannot be empty.");
+        RuleFor(p => p.Category).IsInEnum().WithMessage("Invalid category passed.");
+        RuleFor(p => p.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit price should be between 0 to {double.MaxValue}");
+        RuleFor(p => p.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity should be between 0 to {int.MaxValue}");
+    }
+}
---
+++ Ecommerce.ProductService/Ecommerce.BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace Ecommerce.BusinessLogicLayer.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        RuleFor(p => p.ProductID).NotEmpty().WithMessage("ProductID cannot be empty.");
+        RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product name cannot be empty.");
+        RuleFor(p => p.Category).IsInEnum().WithMessage("Invalid category passed.");
+        RuleFor(p => p.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit price should be between 0 to {double.MaxValue}");
+        RuleFor(p => p.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity should be between 0 to {int.MaxValue}");
+    }
+}
