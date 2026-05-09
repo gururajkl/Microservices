90f09b3e31a9dcf817530c46438dcb6259623b97
gururajkl <gururajragavendra7@gmail.com>
2026-05-09 13:41:04 +0530

    Create Product entity

---
+++ Ecommerce.ProductService/Ecommerce.DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.DataAccessLayer.Entities;
+
+/// <summary>
+/// Product entity represents a product in the microservice.
+/// </summary>
+public class Product
+{
+    [Key]
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public double? UnitPrice { get; set; }
+    public int? QuantityInStock { get; set; }
+}
