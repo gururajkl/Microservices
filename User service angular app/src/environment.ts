5a79a64facdc0a56f7492b1ee0d7b06cc0542ca7
gururajkl <gururajragavendra7@gmail.com>
2026-05-10 17:32:09 +0530

    Update angular app to include product components

--- User service angular app/src/environment.ts
+++ User service angular app/src/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7282/api/auth/', // API base URL for development.
+    apiUrl: 'https://localhost:7282/api/auth/',
+    productsMicroserviceUrl: 'https://localhost:7283/api/products/'
   };
   
