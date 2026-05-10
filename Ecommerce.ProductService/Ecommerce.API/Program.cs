33fad813a766933783d2e50423b1d7f4896e0c4e
gururajkl <gururajragavendra7@gmail.com>
2026-05-10 10:04:33 +0530

    Add swagger support to the product microservice

--- Ecommerce.ProductService/Ecommerce.API/Program.cs
+++ Ecommerce.ProductService/Ecommerce.API/Program.cs
@@ -10,11 +10,20 @@ builder.Services.AddDataAccessLayer(builder.Configuration);
 // Add business logic layer services.
 builder.Services.AddBusinessLogicLayer();
 
+// Add Swagger generation services to the dependency injection container to create swagger specification.
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.UseExceptionHandler();
 
 app.UseRouting();
+
+// Adds endpoint that can serve the swagger.json file.
+app.UseSwagger();
+
+// Adds the swagger ui which can be used to test the API endpoints and view the API documentation.
+app.UseSwaggerUI();
 
 app.MapProductAPIEndpoints();
 
