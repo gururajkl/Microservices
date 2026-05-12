cc37148be17bd4f07cc1eeff009798ced3cfb2ff
gururajkl <gururajragavendra7@gmail.com>
2026-05-12 21:02:54 +0530

    Update appSettings file to include connection string values from env variables

--- Ecommerce.ProductService/Ecommerce.DataAccessLayer/DependencyInjection.cs
+++ Ecommerce.ProductService/Ecommerce.DataAccessLayer/DependencyInjection.cs
@@ -4,10 +4,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        // Register ApplicationDbContext with MySQL provider and connection string from configuration
+        string connectionStringFromJSON = configuration.GetConnectionString("MySQLConnectionString")!;
+
+        // Replace with environment values.
+        string connectionString = connectionStringFromJSON.Replace("$MYSQL_HOSTNAME", Environment.GetEnvironmentVariable("MYSQL_HOSTNAME"))
+            .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+
+        // Register ApplicationDbContext with MySQL provider and connection string from configuration.
         services.AddDbContext<ApplicationDbContext>(options =>
         {
-            options.UseMySQL(configuration.GetConnectionString("MySQLConnectionString")!);
+            options.UseMySQL(connectionString);
         });
 
         services.AddScoped<IProductsRepository, ProductsRepository>();
