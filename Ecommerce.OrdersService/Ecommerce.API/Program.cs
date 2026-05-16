02f7dafaf75de4d0e4097325620e19448ed2c178
gururajkl <gururajragavendra7@gmail.com>
2026-05-16 19:30:39 +0530

    Add swagger support to the orders microservice

--- Ecommerce.OrdersService/Ecommerce.API/Program.cs
+++ Ecommerce.OrdersService/Ecommerce.API/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger generation services to the dependency injection container to create swagger specification.
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+if (app.Environment.IsDevelopment())
+{
+    // Adds endpoint that can serve the swagger.json file.
+    app.UseSwagger();
+
+    // Adds the swagger ui which can be used to test the API endpoints and view the API documentation.
+    app.UseSwaggerUI();
+}
 
 app.Run();
