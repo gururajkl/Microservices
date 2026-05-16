c3cd0a5b9c3a35d4daca4ec36938323e8fa3034b
gururajkl <gururajragavendra7@gmail.com>
2026-05-16 19:35:23 +0530

    Add DependencyInjection for business logic layer
    - Register it in API project

--- Ecommerce.OrdersService/Ecommerce.API/Program.cs
+++ Ecommerce.OrdersService/Ecommerce.API/Program.cs
@@ -1,7 +1,12 @@
+using Ecommerce.BusinessLogicLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Swagger generation services to the dependency injection container to create swagger specification.
 builder.Services.AddSwaggerGen();
+
+// Add business logic layer services.
+builder.Services.AddBusinessLogicLayer();
 
 var app = builder.Build();
 
---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.BusinessLogicLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+    {
+        return services;
+    }
+}
