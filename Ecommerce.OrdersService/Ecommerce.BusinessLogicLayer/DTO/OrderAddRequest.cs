37d575998f48bd746154cc0c4040becc5d76cc8f
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 15:43:15 +0530

    Create OrderAddRequest.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/DTO/OrderAddRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Ecommerce.BusinessLogicLayer.DTO;
+
+public record OrderAddRequest(Guid UserID, DateTime OrderDate, List<OrderItemAddRequest> OrderItems)
+{
+    public OrderAddRequest() : this(default, default, default!) { }
+}
