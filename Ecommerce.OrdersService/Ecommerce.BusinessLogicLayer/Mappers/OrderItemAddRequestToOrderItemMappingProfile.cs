94f5f00cc88986ac5ccea2bcef8e3496d5f5b458
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 19:00:29 +0530

    Create OrderItemAddRequestToOrderItemMappingProfile.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Mappers/OrderItemAddRequestToOrderItemMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Ecommerce.BusinessLogicLayer.DTO;
+using Ecommerce.DataAccessLayer.Entities;
+
+namespace Ecommerce.BusinessLogicLayer.Mappers;
+
+public class OrderItemAddRequestToOrderItemMappingProfile : Profile
+{
+    public OrderItemAddRequestToOrderItemMappingProfile()
+    {
+        CreateMap<OrderItemAddRequest, OrderItem>()
+            .ForMember(dest => dest.ProductID, options => options.MapFrom(src => src.ProductID))
+            .ForMember(dest => dest.UnitPrice, options => options.MapFrom(src => src.UnitPrice))
+            .ForMember(dest => dest.Quantity, options => options.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest._id, options => options.Ignore())
+            .ForMember(dest => dest.TotalPrice, options => options.Ignore());
+    }
+}
