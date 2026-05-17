6a183b6ec252350159ee830d5f827440dba520e7
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 17:05:44 +0530

    Create OrderItemAddRequestValidator.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Validators/OrderItemAddRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace Ecommerce.BusinessLogicLayer.Validators;
+
+public class OrderItemAddRequestValidator : AbstractValidator<OrderItemAddRequest>
+{
+    public OrderItemAddRequestValidator()
+    {
+        RuleFor(r => r.ProductID).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(r => r.UnitPrice).NotEmpty().WithMessage("Unit Price is required")
+            .GreaterThan(0).WithMessage("Unit price should be greater than 0");
+        RuleFor(r => r.Quantity).NotEmpty().WithMessage("Quantity cannot be empty")
+            .GreaterThan(0).WithMessage("Quantity should be greater than 0");
+    }
+}
