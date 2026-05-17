51cc4b71ff7ec2a5ab1a327e75133d5de85265ec
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 12:59:52 +0530

    Add data types for the model classes

--- Ecommerce.OrdersService/Ecommerce.DataAccessLayer/Entities/Order.cs
+++ Ecommerce.OrdersService/Ecommerce.DataAccessLayer/Entities/Order.cs
@@ -5,14 +5,16 @@ namespace Ecommerce.DataAccessLayer.Entities;
 
 public class Order
 {
-    // Maps OrderID as the MongoDB document identifier (_id).
     [BsonId]
-    // Stores Guid as a readable string instead of binary UUID.
+    [BsonRepresentation(BsonType.String)]
+    public Guid _id { get; set; }
     [BsonRepresentation(BsonType.String)]
     public Guid OrderID { get; set; }
     [BsonRepresentation(BsonType.String)]
     public Guid UserID { get; set; }
+    [BsonRepresentation(BsonType.String)]
     public DateTime OrderDate { get; set; }
+    [BsonRepresentation(BsonType.Double)]
     public decimal TotalBill { get; set; }
     public List<OrderItem> OrderItems { get; set; } = [];
 }
--- Ecommerce.OrdersService/Ecommerce.DataAccessLayer/Entities/OrderItem.cs
+++ Ecommerce.OrdersService/Ecommerce.DataAccessLayer/Entities/OrderItem.cs
@@ -5,9 +5,15 @@ namespace Ecommerce.DataAccessLayer.Entities;
 
 public class OrderItem
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid _id { get; set; }
     [BsonRepresentation(BsonType.String)]
     public Guid ProductID { get; set; }
+    [BsonRepresentation(BsonType.Double)]
     public decimal UnitPrice { get; set; }
+    [BsonRepresentation(BsonType.Int32)]
     public int Quantity { get; set; }
+    [BsonRepresentation(BsonType.Double)]
     public decimal TotalPrice { get; set; }
 }
