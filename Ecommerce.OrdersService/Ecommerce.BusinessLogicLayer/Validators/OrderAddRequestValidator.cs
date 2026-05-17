b5b5c99610eb3c40288d040909459e92c6d4beb8
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 17:05:25 +0530

    Create OrderAddRequestValidator.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Validators/OrderAddRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace Ecommerce.BusinessLogicLayer.Validators;
+
+public class OrderAddRequestValidator : AbstractValidator<OrderAddRequest>
+{
+    public OrderAddRequestValidator()
+    {
+        RuleFor(r => r.UserID).NotEmpty().WithMessage("User ID is required");
+        RuleFor(r => r.OrderDate).NotEmpty().WithMessage("Order Date is required");
+        RuleFor(r => r.OrderItems).NotEmpty().WithMessage("Order Items cannot be empty");
+    }
+}
