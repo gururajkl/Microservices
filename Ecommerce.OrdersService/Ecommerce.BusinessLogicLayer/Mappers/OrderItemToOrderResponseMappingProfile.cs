21e824ae4a15995dd80a554436f35489034e30e7
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 19:29:09 +0530

    Create OrderItemToOrderResponseMappingProfile.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Mappers/OrderItemToOrderResponseMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using Ecommerce.DataAccessLayer.Entities;
+
+namespace Ecommerce.BusinessLogicLayer.Mappers;
+
+public class OrderItemToOrderResponseMappingProfile : Profile
+{
+    public OrderItemToOrderResponseMappingProfile()
+    {
+        CreateMap<OrderItem, OrderItemResponse>()
+            .ForMember(dest => dest.ProductID, options => options.MapFrom(src => src.ProductID))
+            .ForMember(dest => dest.UnitPrice, options => options.MapFrom(src => src.UnitPrice))
+            .ForMember(dest => dest.Quantity, options => options.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest.TotalPrice, options => options.MapFrom(src => src.TotalPrice));
+    }
+}
