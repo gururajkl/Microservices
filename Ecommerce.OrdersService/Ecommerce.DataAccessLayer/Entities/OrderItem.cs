16b3d855e20028a5d4557a5b40771f67a7205880
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 12:50:18 +0530

    Create OrderItem Entity

---
+++ Ecommerce.OrdersService/Ecommerce.DataAccessLayer/Entities/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ecommerce.DataAccessLayer.Entities;
+
+public class OrderItem
+{
+    [BsonRepresentation(BsonType.String)]
+    public Guid ProductID { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+}
