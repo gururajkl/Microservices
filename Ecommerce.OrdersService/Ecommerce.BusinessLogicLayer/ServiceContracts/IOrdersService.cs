ef52064431e232282d33dc7aafc7ab1909e0e6e8
gururajkl <gururajragavendra7@gmail.com>
2026-05-17 16:50:54 +0530

    Create IOrdersService.cs

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/ServiceContracts/IOrdersService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using Ecommerce.DataAccessLayer.Entities;
+using MongoDB.Driver;
+
+namespace Ecommerce.BusinessLogicLayer.ServiceContracts;
+
+public interface IOrdersService
+{
+    Task<List<OrderResponse?>> GetOrdersAsync();
+    Task<List<OrderResponse?>> GetOrdersByConditionAsync(FilterDefinition<Order> filterDefinition);
+    Task<OrderResponse?> GetOrderByConditionAsync(FilterDefinition<Order> filterDefinition);
+    Task<OrderResponse?> AddOrderAsync(OrderAddRequest request);
+    Task<OrderResponse?> UpdateOrderAsync(OrderUpdateRequest request);
+    Task<bool> DeleteOrderAsync(Guid orderID);
+}
