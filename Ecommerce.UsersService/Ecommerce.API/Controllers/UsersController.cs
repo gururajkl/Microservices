1778143a5d0943ea892c249bea8ed0f13428e9ed
gururajkl <gururajragavendra7@gmail.com>
2026-05-18 18:57:29 +0530

    Create new user controller which exposes get user by id endpoint

---
+++ Ecommerce.UsersService/Ecommerce.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.Core.DTO;
+using Ecommerce.Core.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(IUsersService service) : ControllerBase
+{
+    [HttpGet("{userID:guid}")]
+    public async Task<IActionResult> GetUser(Guid userID)
+    {
+        if (userID == Guid.Empty)
+        {
+            return BadRequest("User id is not valid");
+        }
+
+        UserDTO? user = await service.GetUserByUserID(userID);
+
+        if (user == null)
+        {
+            return NotFound(user);
+        }
+
+        return Ok(user);
+    }
+}
