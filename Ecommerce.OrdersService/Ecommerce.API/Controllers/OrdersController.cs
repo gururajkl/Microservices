abffb7f8963d4c75625bbcde3676aab4a08e2d45
gururajkl <gururajragavendra7@gmail.com>
2026-05-18 14:22:07 +0530

    Create GetOrder endpoint

---
+++ Ecommerce.OrdersService/Ecommerce.API/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.BusinessLogicLayer.DTO;
+using Ecommerce.BusinessLogicLayer.ServiceContracts;
+using Ecommerce.DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Ecommerce.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController(IOrdersService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<OrderResponse?>> GetOrders()
+    {
+        return await service.GetOrdersAsync();
+    }
+
+    [HttpGet("search/order-id/{orderID}")]
+    public async Task<OrderResponse?> GetOrder(Guid orderID)
+    {
+        FilterDefinition<Order> filter = Builders<Order>.Filter.Eq(o => o.OrderID, orderID);
+        return await service.GetOrderByConditionAsync(filter);
+    }
+}
