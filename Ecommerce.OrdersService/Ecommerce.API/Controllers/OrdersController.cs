8772c5f4f27b6fb40bc8dec827e5ac5b6650c3ea
gururajkl <gururajragavendra7@gmail.com>
2026-05-18 14:30:34 +0530

    Create GetOrdersByOrderDate

--- Ecommerce.OrdersService/Ecommerce.API/Controllers/OrdersController.cs
+++ Ecommerce.OrdersService/Ecommerce.API/Controllers/OrdersController.cs
@@ -24,10 +24,17 @@ public class OrdersController(IOrdersService service) : ControllerBase
     }
 
     [HttpGet("search/product-id/{productID}")]
-    public async Task<IEnumerable<OrderResponse?>> GetOrderByProductID(Guid productID)
+    public async Task<IEnumerable<OrderResponse?>> GetOrdersByProductID(Guid productID)
     {
         FilterDefinition<Order> filter = Builders<Order>.Filter.ElemMatch(o => o.OrderItems,
             Builders<OrderItem>.Filter.Eq(ot => ot.ProductID, productID));
+        return await service.GetOrdersByConditionAsync(filter);
+    }
+
+    [HttpGet("search/orderDate/{orderDate}")]
+    public async Task<IEnumerable<OrderResponse?>> GetOrdersByOrderDate(DateTime orderDate)
+    {
+        FilterDefinition<Order> filter = Builders<Order>.Filter.Eq(o => o.OrderDate.ToString("yyyyy-MM-dddd"), orderDate.ToString("yyyyy-MM-dddd"));
         return await service.GetOrdersByConditionAsync(filter);
     }
 }
