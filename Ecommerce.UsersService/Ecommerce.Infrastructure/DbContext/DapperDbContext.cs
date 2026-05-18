95c7589c45ccf7c2723f3d054d9ee08f3310f4f7
gururajkl <gururajragavendra7@gmail.com>
2026-05-18 19:11:28 +0530

    Make all values in connection string as dynamic

--- Ecommerce.UsersService/Ecommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ Ecommerce.UsersService/Ecommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -13,7 +13,10 @@ public class DapperDbContext
     {
         string? connectionStringTemplate = configuration.GetConnectionString("PostgresConnection")!;
         string? connectionString = connectionStringTemplate.Replace("$POSTGRES_HOSTNAME", Environment.GetEnvironmentVariable("POSTGRES_HOSTNAME"))
-            .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
+            .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"))
+            .Replace("$POSTGRES_USER", Environment.GetEnvironmentVariable("POSTGRES_USER"))
+            .Replace("$POSTGRES_DATABASE", Environment.GetEnvironmentVariable("POSTGRES_DATABASE"))
+            .Replace("$POSTGRES_PORT", Environment.GetEnvironmentVariable("POSTGRES_PORT"));
 
         _connection = new NpgsqlConnection(connectionString);
     }
