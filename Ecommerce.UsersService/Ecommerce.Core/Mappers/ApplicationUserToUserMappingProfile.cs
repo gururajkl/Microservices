82aff0fba1087fcb61ca07d32dbd50b85cd744b7
gururajkl <gururajragavendra7@gmail.com>
2026-05-18 18:57:10 +0530

    Add ApplicationUserToUserMappingProfile

---
+++ Ecommerce.UsersService/Ecommerce.Core/Mappers/ApplicationUserToUserMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ecommerce.Core.DTO;
+using Ecommerce.Core.Entities;
+
+namespace Ecommerce.Core.Mappers;
+
+public class ApplicationUserToUserMappingProfile : Profile
+{
+    public ApplicationUserToUserMappingProfile()
+    {
+        CreateMap<ApplicationUser, UserDTO>()
+            .ForMember(dest => dest.UserID, options => options.MapFrom(src => src.UserID))
+            .ForMember(dest => dest.Email, options => options.MapFrom(src => src.Email))
+            .ForMember(dest => dest.PersonName, options => options.MapFrom(src => src.PersonName))
+            .ForMember(dest => dest.Gender, options => options.MapFrom(src => src.Gender));
+    }
+}
