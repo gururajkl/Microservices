a2275a608d571d2e612d57eef3471b2f0e6f0281
gururajkl <gururajragavendra7@gmail.com>
2026-05-25 20:41:19 +0530

    Rename UserId to UserID and update mapping/config
    Renamed the UserId property to UserID in AuthenticationResponse for consistency. Updated XML docs and mapping profile accordingly. Added a parameterless constructor to AuthenticationResponse for serialization and automapper support.

--- Ecommerce.UsersService/Ecommerce.Core/DTO/AuthenticationResponse.cs
+++ Ecommerce.UsersService/Ecommerce.Core/DTO/AuthenticationResponse.cs
@@ -3,13 +3,13 @@
 /// <summary>
 /// Record class representing the authentication response details returned to the user after successful or failed login or registration.
 /// </summary>
-/// <param name="UserId">User id.</param>
+/// <param name="UserID">User id.</param>
 /// <param name="Email">Email of the user.</param>
 /// <param name="PersonName">Name of the user.</param>
 /// <param name="Gender">Gender of the user.</param>
 /// <param name="Token">Authentication token.</param>
 /// <param name="Success">Indicates whether the authentication was successful.</param>
-public record AuthenticationResponse(Guid UserId, string? Email, string? PersonName, string? Gender, string? Token, bool Success)
+public record AuthenticationResponse(Guid UserID, string? Email, string? PersonName, string? Gender, string? Token, bool Success)
 {
     // Parameterless constructor for serialization purposes for auto mapper.
     public AuthenticationResponse() : this(default, default, default, default, default, default) { }
