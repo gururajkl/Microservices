15742fcfe3c640a49c60fa4a99cc688ba3c60c2c
gururajkl <gururajragavendra7@gmail.com>
2026-05-25 10:35:18 +0530

    Create new order components and call the backend apis

--- AngularApplication/src/environment.ts
+++ AngularApplication/src/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7282/api/auth/',
-    productsMicroserviceUrl: 'https://localhost:7283/api/products/'
+    usersAPIURL: 'http://localhost:5000/api/',
+    productsAPIURL: 'http://localhost:6000/api/products/',
+    ordersAPIURL: 'http://localhost:7000/api/orders/',
   };
-  
