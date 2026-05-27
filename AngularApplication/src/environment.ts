9a26436c8da229e58643f314af63d0ae3731469e
gururajkl <gururajragavendra7@gmail.com>
2026-05-27 19:39:56 +0530

    Update http ports of microservices

--- AngularApplication/src/environment.ts
+++ AngularApplication/src/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    usersAPIURL: 'http://localhost:5000/api/',
-    productsAPIURL: 'http://localhost:6000/api/products/',
-    ordersAPIURL: 'http://localhost:7000/api/orders/',
+    usersAPIURL: 'http://localhost:5001/api/',
+    productsAPIURL: 'http://localhost:6001/api/products/',
+    ordersAPIURL: 'http://localhost:7001/api/orders/',
   };
