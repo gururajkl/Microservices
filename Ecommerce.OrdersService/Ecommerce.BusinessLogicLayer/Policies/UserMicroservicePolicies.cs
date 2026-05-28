b30250370b1408c115a32d82e22f83f4f2903756
gururajkl <gururajragavendra7@gmail.com>
2026-05-28 14:22:12 +0530

    Update retry attempt to 3 and fix comments

--- Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Policies/UserMicroservicePolicies.cs
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/Policies/UserMicroservicePolicies.cs
@@ -7,12 +7,12 @@ namespace Ecommerce.BusinessLogicLayer.Policies;
 
 public class UserMicroservicePolicies(ILogger<UserMicroservicePolicies> logger) : IUserMicroservicePolicies
 {
-    // The policy will retry the request up to 5 times with a delay of 3 seconds between each retry if the response is not successful.
+    // The policy will retry the request up to 3 times with a delay of 2 seconds between each retry if the response is not successful.
     public IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
     {
         AsyncRetryPolicy<HttpResponseMessage> policy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
             // Math.Pow(2, retryAttempt) will create an exponential backoff strategy.
-            .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), onRetry: (response, timespan, retryCount, context) =>
+            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), onRetry: (response, timespan, retryCount, context) =>
             {
                 logger.LogInformation("Retrying request to users microservice. Attempt {RetryCount}. Waiting {TimeSpan} before next retry", retryCount,
                     timespan);
