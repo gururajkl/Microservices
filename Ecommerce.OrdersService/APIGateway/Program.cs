9e6fd47d20b23852b072853e2491a0ebe748ac6b
gururajkl <gururajragavendra7@gmail.com>
2026-05-29 19:17:03 +0530

    Make ocelot json template ready

--- Ecommerce.OrdersService/APIGateway/Program.cs
+++ Ecommerce.OrdersService/APIGateway/Program.cs
@@ -1,6 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Add ocelot configuration file.
+builder.Configuration.AddJsonFile("oclot.json", false, true);
+
+// Add ocelot services to the container.
+builder.Services.AddOcelot();
 
 app.Run();
+
+// Use ocelot middleware to handle incoming requests and route them to the appropriate downstream services.
+// This is asynchronous so await is used to ensure that the application waits for the middleware to be fully set up before processing requests.
+await app.UseOcelot();
