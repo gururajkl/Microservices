c091a6d7eb7263afe9c792b33cc2bfe1bd130f87
gururajkl <gururajragavendra7@gmail.com>
2026-05-30 13:02:56 +0530

    Update users initial script file to include dummy user data

--- DatabaseQuery/users-init/db2.sql
+++ DatabaseQuery/users-init/db2.sql
@@ -8,3 +8,9 @@ CREATE TABLE public."Users"
     "Gender" character varying(15) COLLATE pg_catalog."default",
     CONSTRAINT "Users_pkey" PRIMARY KEY ("UserID")
 );
+
+-- Sample data for insertion
+INSERT INTO public."Users" ("UserID", "Email", "PersonName", "Gender", "Password")
+VALUES 
+('c32f8b42-60e6-4c02-90a7-9143ab37189f', 'test1@example.com', 'John Doe', 'Male', 'password1'),
+('8ff22c7d-18c7-4ef0-a0ac-988ecb2ac7f5', 'test2@example.com', 'Jane Smith', 'Female', 'password2');
