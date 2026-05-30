a6930ef494a3eec438d9ba1e464dd56cf0b0bd9d
gururajkl <gururajragavendra7@gmail.com>
2026-05-30 08:44:11 +0530

    Fix order of terminal middleware in gat way project

--- Ecommerce.OrdersService/APIGateway/Program.cs
+++ Ecommerce.OrdersService/APIGateway/Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-app.Run();
-
 // Use ocelot middleware to handle incoming requests and route them to the appropriate downstream services.
 // This is asynchronous so await is used to ensure that the application waits for the middleware to be fully set up before processing requests.
 await app.UseOcelot();
+
+app.Run();
