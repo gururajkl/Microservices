d5fc55c6d6ba8113d966ba156ff3e02f0685c64b
gururajkl <gururajragavendra7@gmail.com>
2026-05-30 19:14:34 +0530

    Route Angular API calls through gateway

--- AngularApplication/src/environment.ts
+++ AngularApplication/src/environment.ts
@@ -1,7 +1,8 @@
 // src/environments/environment.ts
 export const environment = {
-    production: false,
-    usersAPIURL: 'http://localhost:5001/api/',
-    productsAPIURL: 'http://localhost:6001/api/products/',
-    ordersAPIURL: 'http://localhost:7001/api/orders/',
-  };
+  production: false,
+  usersAPIURL: 'http://localhost:5000/gateway/users/',
+  productsAPIURL: 'http://localhost:5000/gateway/products/',
+  ordersAPIURL: 'http://localhost:5000/gateway/orders/',
+  gatewayURL: 'http://localhost:5000/gateway/',
+};
