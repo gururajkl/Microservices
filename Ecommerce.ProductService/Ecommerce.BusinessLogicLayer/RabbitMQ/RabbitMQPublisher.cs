e9b3952852ba37b914cad11d50fd846611401ee6
gururajkl <gururajragavendra7@gmail.com>
2026-05-31 19:45:49 +0530

    Create RabbitMQPublisher implementation

---
+++ Ecommerce.ProductService/Ecommerce.BusinessLogicLayer/RabbitMQ/RabbitMQPublisher.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace Ecommerce.BusinessLogicLayer.RabbitMQ;
+
+public class RabbitMQPublisher : IRabbitMQPublisher, IDisposable
+{
+    private readonly IConfiguration _configuration;
+    private IModel _channel;
+    private IConnection _connection;
+
+    public RabbitMQPublisher(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        string hostName = _configuration["RABBITMQ_HostName"]!;
+        string port = _configuration["RABBITMQ_Port"]!;
+        string userName = _configuration["RABBITMQ_UserName"]!;
+        string password = _configuration["RABBITMQ_Password"]!;
+
+        ConnectionFactory connectionFactory = new ConnectionFactory()
+        {
+            HostName = hostName,
+            Port = int.Parse(port),
+            UserName = userName,
+            Password = password
+        };
+
+        _connection = connectionFactory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
+
+    public void Publish<T>(string routingKey, T message)
+    {
+        string exchangeName = "products.exchange";
+
+        // RabbitMQ cannot send complex objects, so we need to serialize the message to JSON and then convert it to bytes.
+        string messageInJSON = JsonSerializer.Serialize(message);
+        byte[] messageInBytes = Encoding.UTF8.GetBytes(messageInJSON);
+
+        // Create the exchange if it doesn't exist. This is idempotent, so it won't create a new exchange if it already exists.
+        _channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, durable: true);
+
+        // Publish the message to the exchange with the specified routing key.
+        _channel.BasicPublish(exchangeName, routingKey, null, messageInBytes);
+    }
+
+    public void Dispose()
+    {
+        _channel.Dispose();
+        _connection.Dispose();
+    }
+}
