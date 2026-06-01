514d4215dd895e02c41f91004a3b3074419f78f9
gururajkl <gururajragavendra7@gmail.com>
2026-06-01 16:22:11 +0530

    Create product delete rabbitmq host service

---
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/RabbitMQ/Services/RabbitMQProductDeleteHostedService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Ecommerce.BusinessLogicLayer.RabbitMQ.Services;
+
+public class RabbitMQProductDeleteHostedService(IRabbitMQProductDeleteConsumer consumer) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        consumer.Consume();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        consumer.Dispose();
+        return Task.CompletedTask;
+    }
+}
