b71332f9ba5b340fd3ee25784ae959fd29975489
gururajkl <gururajragavendra7@gmail.com>
2026-06-01 19:00:19 +0530

    Remove deleted product from cache in delete product consumer

--- Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/RabbitMQ/RabbitMQProductDeleteConsumer.cs
+++ Ecommerce.OrdersService/Ecommerce.BusinessLogicLayer/RabbitMQ/RabbitMQProductDeleteConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.BusinessLogicLayer.RabbitMQ.MessageTypes;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
@@ -14,11 +15,13 @@ public class RabbitMQProductDeleteConsumer : IRabbitMQProductDeleteConsumer, IDi
     private readonly IModel _channel;
     private readonly IConnection _connection;
     private readonly ILogger<RabbitMQProductDeleteConsumer> _logger;
+    private readonly IDistributedCache _cache;
 
-    public RabbitMQProductDeleteConsumer(IConfiguration configuration, ILogger<RabbitMQProductDeleteConsumer> logger)
+    public RabbitMQProductDeleteConsumer(IConfiguration configuration, ILogger<RabbitMQProductDeleteConsumer> logger, IDistributedCache cache)
     {
         _configuration = configuration;
         _logger = logger;
+        _cache = cache;
 
         string hostName = _configuration["RABBITMQ_HostName"]!;
         string port = _configuration["RABBITMQ_Port"]!;
@@ -56,14 +59,12 @@ public class RabbitMQProductDeleteConsumer : IRabbitMQProductDeleteConsumer, IDi
 
         EventingBasicConsumer consumer = new EventingBasicConsumer(_channel);
 
-        consumer.Received += (sender, args) =>
+        consumer.Received += async (sender, args) =>
         {
             byte[] body = args.Body.ToArray();
             string stringMessage = Encoding.UTF8.GetString(body);
             ProductDeleteMessage message = JsonSerializer.Deserialize<ProductDeleteMessage>(stringMessage)!;
-
-            _logger.LogInformation("Received ProductDeleteMessage: ProductID={ProductID}, ProductName={ProductName}",
-                message.ProductID, message.ProductName);
+            await HandleProductUpdate(message.ProductID);
         };
 
         _channel.BasicConsume(queueName, true, consumer);
@@ -73,5 +74,12 @@ public class RabbitMQProductDeleteConsumer : IRabbitMQProductDeleteConsumer, IDi
     {
         _channel.Dispose();
         _connection.Dispose();
+    }
+
+    private async Task HandleProductUpdate(Guid productID)
+    {
+        _logger.LogInformation("Received product delete message for product ID: {ProductID}", productID);
+        string cachedKey = $"product:{productID}";
+        await _cache.RemoveAsync(cachedKey);
     }
 }
